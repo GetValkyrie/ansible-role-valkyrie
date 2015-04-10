442cb46d421a75a117b837d06258af40c09e4539
Christopher Gervais <chris@ergonlogic.com>
2015-04-10 15:51:50 -0400

    Install Skynet role for tests.

--- tests/setup.sh
+++ tests/setup.sh
@@ -2,6 +2,7 @@
 
 curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh 2>/dev/null | /bin/sh
 ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/ansible-role-aegir,,getvalkyrie.aegir
+ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/ansible-role-skynet,,getvalkyrie.skynet
 target=/etc/ansible/roles/getvalkyrie.valkyrie
 if [ ! -e $target ]; then
   ln -s /vagrant/ $target
